52ae1321420b906374219ee13cd4dc0450ed3e82
8bitPandaPlugins <45884278+8bitPandaPlugins@users.noreply.github.com>
2019-03-14 15:35:53 +0000

    changed constructor added player image

--- RPG/Sprite.h
+++ RPG/Sprite.h
@@ -7,20 +7,14 @@ public:
 	Sprite(std::string imgPath, Display Graphics):Sheet(imgPath,Graphics.getRender()),mGraphics(&Graphics)
 	{
 	}
-	Sprite(bool test, Display Graphics):mGraphics(&Graphics)
-	{
-	
-
-
-	}
 	~Sprite(){}
 	virtual void update()
 	{
 
 	}
-	void draw(Display *Graphics)
+	void draw()
 	{
-		
+		mGraphics->drawMobTexture(0, Sheet, x, y);
 	}
 
 private:
