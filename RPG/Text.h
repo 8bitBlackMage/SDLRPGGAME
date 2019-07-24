8491e3702db08c583405a03ff30453e742056dd9
8bitPandaPlugins <45884278+8bitPandaPlugins@users.noreply.github.com>
2019-07-24 19:40:54 +0100

    added text class

--- RPG/Text.h
+++ RPG/Text.h
@@ -1,6 +1,27 @@
 #pragma once 
-
-
+#include "Display.h"
+#include "SDL_ttf.h"
+#include <string>
 class TextHandler {
 
+	TextHandler() {
+		if (TTF_Init() < 0) {
+			abort();
+		}
+	}
+	TextHandler(std::string pathtoFont): FontPath(pathtoFont){
+		if (TTF_Init() < 0) {
+			abort();
+		}
+	}
+	void loadFont(std::string pathtoFont) {
+		GameFont = TTF_OpenFont(pathtoFont.c_str(), 20);
+	}
+	
+
+
+
+	TTF_Font * GameFont;
+	std::string FontPath;
+//this class handles all the text drawing and 
 };
